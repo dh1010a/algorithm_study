ffe09b250952b35c8ae5c1ef47f73fa7ebbdcc90
dh1010a <dh1010a@naver.com>
2024-11-12 01:16:22 +0900

    add: swea 1220

--- sw_academy/Main.java
+++ sw_academy/Main.java
@@ -3,83 +3,38 @@ import java.io.*;
 
 class Main{
     public static void main(String args[]) throws Exception {
-        Scanner sc = new Scanner(System.in);
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T;
         T=1;
 
         for(int tc = 1; tc <= T; tc++) {
-            int N = sc.nextInt();
+            int N = Integer.parseInt(br.readLine());
+            int[][] table = new int[N][N];
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                for (int j = 0; j < N; j++) {
+                    table[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
             int result = 0;
+            for (int y = 0; y < N; y++) {
+                int last = 0;
+                for (int x = 0; x < N; x++) {
+                    if (table[x][y] == 1) {
+                        last = 1;
+                    } else if (table[x][y] == 2) {
 
-            char[][] arr = new char[8][];
-            for (int i = 0; i < 8; i++) {
-                arr[i] = sc.next().toCharArray();
-            }
-
-            // 세로 회문 먼저
-            for (int y = 0; y < 8; y++) {
-                int start = 0;
-                int end = start + N - 1;
-                StringBuilder str = new StringBuilder();
-                for (int i = start; i <= end; i++) {
-                    str.append(arr[i][y]);
-                }
-                while (true) {
-                    if (isReverseable(N, str.toString().toCharArray())) {
-                        result++;
+                        if (last == 1) {
+                            result++;
+                        }
+                        last = 2;
                     }
-                    start++;
-                    end++;
-                    if (end >= 8) {
-                        break;
-                    }
-                    str.deleteCharAt(0);
-                    str.append(arr[end][y]);
-                }
-            }
-
-            //가로 회문
-            for (int x = 0; x < 8; x++) {
-                int start = 0;
-                int end = start + N - 1;
-                StringBuilder str = new StringBuilder();
-                for (int i = start; i <= end; i++) {
-                    str.append(arr[x][i]);
-                }
-                while (true) {
-                    if (isReverseable(N, str.toString().toCharArray())) {
-                        result++;
-                    }
-                    start++;
-                    end++;
-                    if (end >= 8) {
-                        break;
-                    }
-                    str.deleteCharAt(0);
-                    str.append(arr[x][end]);
                 }
             }
 
             System.out.println("#" + tc + " " + result);
         }
-    }
-
-
-    public static boolean isReverseable(int N, char[] arr) {
-        int mid = N / 2;
-        if (N % 2 == 0) {
-            for (int i = mid; i < N; i++) {
-                if (arr[N - i - 1] != arr[i]) {
-                    return false;
-                }
-            }
-        } else {
-            for (int i = mid + 1; i < N; i++) {
-                if (arr[N - i - 1] != arr[i]) {
-                    return false;
-                }
-            }
-        }
-        return true;
     }
 }
---
+++ sw_academy/swea1220.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class swea1220 {
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T;
+        T=10;
+
+        for(int tc = 1; tc <= T; tc++) {
+            int N = Integer.parseInt(br.readLine());
+            int[][] table = new int[N][N];
+
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+                for (int j = 0; j < N; j++) {
+                    table[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            int result = 0;
+            for (int y = 0; y < N; y++) {
+                int x = 0;
+                while (table[x][y] != 1) {
+                    if (table[x][y] == 2) {
+                        result++;
+                    }
+                    x++;
+                }
+            }
+
+            for (int y = 0; y < N; y++) {
+                int x = 99;
+                while (table[x][y] != 2) {
+                    if (table[x][y] == 1) {
+                        result++;
+                    }
+                    x--;
+                }
+            }
+
+            System.out.println("#" + tc + " " + result);
+        }
+    }
+}
