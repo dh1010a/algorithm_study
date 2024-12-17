bf382250b0e3f600b4114289ae9e823f0974779b
dh1010a <dh1010a@naver.com>
2024-12-17 22:17:24 +0900

    add: boj9251, 11052

--- BOJ/Java/DP/boj11052.java
+++ BOJ/Java/DP/boj11052.java
@@ -1,2 +1,36 @@
-package DP;public class boj11052 {
+package DP;
+
+import java.util.*;
+import java.io.*;
+
+class boj11052 {
+
+	public static void main(String args[]) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(br.readLine());
+
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+		int[] card = new int[N + 1];
+
+		for (int i = 1; i <= N; i++) {
+			card[i] = Integer.parseInt(st.nextToken());
+		}
+
+		int[] dp = new int[N + 1];
+		dp[1] = card[1];
+		for (int i = 2; i <= N; i++) {
+			int mid = i / 2;
+			if (i % 2 != 0) {
+				mid++;
+			}
+			for (int j = mid; j <= i; j++) {
+				dp[i] = Math.max(Math.max(dp[j] + dp[i - j], card[i]), dp[i]);
+			}
+		}
+		System.out.println(dp[N]);
+
+	}
+
+
 }
