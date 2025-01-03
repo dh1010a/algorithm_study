875089dac2b0158623e2a451096b6982f8fcab1b
dh1010a <dh1010a@naver.com>
2025-01-03 18:11:16 +0900

    0103

--- BOJ/Java/boj17069.java
+++ BOJ/Java/boj17069.java
@@ -21,19 +21,19 @@ class boj17069 {
 			for (int j = 2; j < n; j++) {
 				// 가로
 				if (j - 1 >= 0 && map[i][j] == 0) {
-					dp[i][j][0] = dp[i][j-1][0] + dp[i][j-1][2];
+					dp[i][j][0] = dp[i][j - 1][0] + dp[i][j - 1][2];
 				}
 				// 세로
 				if (i - 1 >= 0 && map[i][j] == 0) {
-					dp[i][j][1] = dp[i-1][j][1] + dp[i-1][j][2];
+					dp[i][j][1] = dp[i - 1][j][1] + dp[i - 1][j][2];
 				}
 				// 대각
-				if (i - 1 >= 0 && j -1 >= 0 && map[i][j-1] == 0 && map[i-1][j] == 0 && map[i-1][j-1] == 0) {
-					dp[i][j][2] = dp[i-1][j-1][0] + dp[i-1][j-1][1] + dp[i-1][j-1][2];
+				if (i - 1 >= 0 && j - 1 >= 0 && map[i][j - 1] == 0 && map[i - 1][j] == 0 && map[i - 1][j - 1] == 0) {
+					dp[i][j][2] = dp[i - 1][j - 1][0] + dp[i - 1][j - 1][1] + dp[i - 1][j - 1][2];
 				}
 			}
 		}
-		long answer = dp[n-1][n-1][0] + dp[n-1][n-1][1] + dp[n-1][n-1][2];
+		long answer = dp[n - 1][n - 1][0] + dp[n - 1][n - 1][1] + dp[n - 1][n - 1][2];
 		System.out.println(answer);
 
 	}
