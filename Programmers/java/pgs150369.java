a99f8d951cef545f6d46dc17a000a977b95055c4
dh1010a <dh1010a@naver.com>
2025-08-02 17:26:23 +0900

    add: pgs 150369

---
+++ Programmers/java/pgs150369.java
@@ -0,0 +1,36 @@
+import java.util.*;
+class pgs150369 {
+
+	long answer = 0;
+
+	public long solution(int cap, int n, int[] deliveries, int[] pickups) {
+
+		for (int i = n - 1; i >= 0; ) {
+
+			if (deliveries[i] == 0 && pickups[i] == 0) {
+				i--;
+				continue;
+			}
+
+			calculate(cap, deliveries, i);
+			calculate(cap, pickups, i);
+
+			answer += (i + 1) * 2;
+		}
+		return answer;
+	}
+
+	private void calculate(int cap, int[] arr, int idx) {
+
+		while (idx >= 0) {
+			if (cap >= arr[idx]) {
+				cap -= arr[idx];
+				arr[idx] = 0;
+				idx--;
+			} else {
+				arr[idx] -= cap;
+				break;
+			}
+		}
+	}
+}
