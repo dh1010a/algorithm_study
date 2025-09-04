4e4b4808c1c5c293d5a07345a22f89bbbcbe232c
dh1010a <dh1010a@naver.com>
2025-09-04 22:55:42 +0900

    add: pgs178870

---
+++ Programmers/java/pgs178870.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class pgs178870 {
+	public int[] solution(int[] sequence, int k) {
+		int[] answer = {0, 1_000_000};
+		int sum = 0;
+		int start = 0;
+
+		for (int end = 0; end < sequence.length; end++) {
+			sum += sequence[end];
+
+			while (sum > k) {
+				sum -= sequence[start];
+				start++;
+			}
+
+			if (sum == k && end - start < answer[1] - answer[0]) {
+				answer[0] = start;
+				answer[1] = end;
+			}
+
+
+		}
+
+		return answer;
+	}
+}
