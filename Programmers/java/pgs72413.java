9afa032e4cedeaf6b177bf262c176cc345749acf
dh1010a <dh1010a@naver.com>
2025-09-21 15:36:07 +0900

    resolve: pgs72413

--- Programmers/java/pgs72413.java
+++ Programmers/java/pgs72413.java
@@ -1,45 +1,101 @@
-import java.util.*;
-class Solution {
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+
+class pgs72413 {
+
+
     public int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = 0;
-        int[][] graph = new int[n+1][n+1];
-        for (int[] x: fares) {
+        int[][] graph = new int[n + 1][n + 1];
+
+
+        for (int[] x : fares) {
             graph[x[0]][x[1]] = x[2];
             graph[x[1]][x[0]] = x[2];
         }
-        
-        int[][] cost = new int[3][n+1];
-        for (int i = 0; i < 3; i++)
-            Arrays.fill(cost[i], Integer.MAX_VALUE);
-        
+        int[][] costs = new int[3][n + 1];
+        for (int[] x : costs) {
+            Arrays.fill(x, Integer.MAX_VALUE);
+        }
+
+        Deque<int[]> dq = new ArrayDeque<>();
         int[] target = {s, a, b};
         for (int i = 0; i < 3; i++) {
-            Queue<int[]> pq = new PriorityQueue<>((o1, o2) -> o1[1] - o2[1]);
-            int start = target[i];
-            cost[i][start] = 0;
-            pq.add(new int[]{start, 0});
-            
-            while (!pq.isEmpty()) {
-                int[] now = pq.poll();
-                
-                for (int j = 1; j <= n; j++) {
+            dq.add(new int[]{target[i], 0});
+            costs[i][target[i]] = 0;
+            while (!dq.isEmpty()) {
+                int[] now = dq.poll();
+
+                for (int j = 1; j < n + 1; j++) {
+//					if (j == now[0]) continue;
                     if (graph[now[0]][j] == 0) continue;
-                    if (cost[i][j] > now[1] + graph[now[0]][j]) {
-                        cost[i][j] = now[1] + graph[now[0]][j];
-                        pq.add(new int[]{j, cost[i][j]});
+
+                    if (costs[i][j] > now[1] + graph[now[0]][j]) {
+                        costs[i][j] = now[1] + graph[now[0]][j];
+                        dq.add(new int[]{j, costs[i][j]});
                     }
+                }
+
+            }
+        }
+
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 1; i < n + 1; i++) {
+            int sum = 0;
+
+            for (int j = 0; j < 3; j++) {
+                sum += costs[j][i];
+            }
+            answer = Math.min(sum, answer);
+        }
+
+
+        return answer;
+    }
+
+
+}
+
+// 플로이드 와샬 알고리즘
+
+class Solution {
+
+
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        int[][] graph = new int[n + 1][n + 1];
+
+        for (int i = 1 ; i < n + 1; i++) {
+            Arrays.fill(graph[i], 200 * 100_000);
+            graph[i][i] = 0;
+        }
+
+        for (int[] x : fares) {
+            graph[x[0]][x[1]] = x[2];
+            graph[x[1]][x[0]] = x[2];
+        }
+
+
+
+        for (int k = 1; k < n + 1; k++) {
+            for (int i = 1; i < n + 1; i++) {
+                for (int j = 1; j < n + 1; j++) {
+                    if (graph[i][j] > graph[i][k] + graph[k][j]) {
+                        graph[i][j] = graph[i][k] + graph[k][j];
+                    }
+
                 }
             }
         }
-        answer = Integer.MAX_VALUE;
-        for (int i = 1; i <= n; i++) {
-            int sum = 0;
-            for (int[] x : cost) {
-                sum += x[i];
-            }
-            answer = Math.min(answer, sum);
+
+        int answer = Integer.MAX_VALUE;
+
+        for (int i = 1; i < n + 1; i++) {
+            answer = Math.min(answer, graph[s][i] + graph[i][a] + graph[i][b]);
         }
-        
+
         return answer;
     }
+
+
 }
