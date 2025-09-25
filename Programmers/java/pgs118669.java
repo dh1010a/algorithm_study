842c43ae59093eaf7bdb2d55b1a7f2ea51c68a25
dh1010a <dh1010a@naver.com>
2025-09-26 00:30:14 +0900

    resolve: pgs118669

--- Programmers/java/pgs118669.java
+++ Programmers/java/pgs118669.java
@@ -1,73 +1,72 @@
 import java.util.*;
 
-class Solution {
-    
-    class Edge implements Comparable<Edge> {
+class pgs118669 {
+    public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
+        Map<Integer, List<Edge>> graph = new HashMap<>();
+        Set<Integer> summitSet = new HashSet<>();
+        for (int x : summits) {
+            summitSet.add(x);
+        }
+
+        for (int[] path : paths) {
+            graph.putIfAbsent(path[0], new LinkedList<>());
+            graph.putIfAbsent(path[1], new LinkedList<>());
+
+            graph.get(path[0]).add(new Edge(path[1], path[2]));
+            graph.get(path[1]).add(new Edge(path[0], path[2]));
+        }
+
+        int[] intensities = new int[n + 1];
+        Arrays.fill(intensities, 10_000_001);
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+
+        for (int start : gates) {
+            pq.add(new Edge(start, 0));
+            intensities[start] = 0;
+        }
+
+        while (!pq.isEmpty()) {
+            Edge now = pq.poll();
+            if (summitSet.contains(now.node)|| now.intense > intensities[now.node]) continue;
+
+            for (Edge next : graph.get(now.node)) {
+                int nextIntense = Math.max(now.intense, next.intense);
+                if (intensities[next.node] > nextIntense) {
+                    intensities[next.node] = nextIntense;
+                    pq.add(new Edge(next.node, nextIntense));
+                }
+            }
+        }
+
+        int[] answer = {500000, 10_000_001};
+        for (int summit : summits) {
+            if (answer[1] == intensities[summit]) {
+                answer[0] = Math.min(answer[0], summit);
+            } else if (answer[1] > intensities[summit]) {
+                answer[0] = summit;
+                answer[1] = intensities[summit];
+            }
+        }
+
+        return answer;
+    }
+
+
+    class Edge implements Comparable<Edge>{
+
         int node;
         int intense;
-        
+
         public Edge(int node, int intense) {
             this.node = node;
             this.intense = intense;
         }
-        
+
         @Override
         public int compareTo(Edge diff) {
             return this.intense - diff.intense;
         }
     }
-    
-    Map<Integer, List<Edge>> map = new HashMap<>();
-    Set<Integer> set = new HashSet<>();
-    
-    public int[] solution(int n, int[][] paths, int[] gates, int[] summits) {
-        int[] answer = new int[2];
-        // Arrays.sort(summits);
-        answer[1] = Integer.MAX_VALUE;
-        for (int x : summits) {
-            set.add(x);
-        }
-        
-        for (int i = 1; i <= n; i++) {
-            map.put(i, new ArrayList<>());
-        }
-        for (int[] path : paths) {
-            map.get(path[0]).add(new Edge(path[1], path[2]));
-            map.get(path[1]).add(new Edge(path[0], path[2]));
-        }
-        
-        int[] visited = new int[n+1];
-        Arrays.fill(visited, Integer.MAX_VALUE);
-        Queue<Edge> pq = new PriorityQueue<>();
-        for (int x : gates) {
-            pq.add(new Edge(x, 0));
-            visited[x] = 0;
-        }
-        
-        while (!pq.isEmpty()) {
-            Edge now = pq.poll();
-            if (visited[now.node] < now.intense || set.contains(now.node)) continue;
-            
-            for (Edge next : map.get(now.node)) {
-                int nextIntense = Math.max(next.intense, now.intense);
-                if (visited[next.node] > nextIntense){
-                    visited[next.node] = nextIntense;
-                    pq.add(new Edge(next.node, visited[next.node]));
-                }
-            }
-        }
-        for (int x: summits) {
-            // 아래 과정을 하지 않기 위해서는 sort를 해야함
-            if (visited[x] == answer[1]) {
-                if (answer[0] > x) {
-                    answer[0] = x;
-                }
-            }
-            else if (visited[x] < answer[1]) {
-                answer[0] = x;
-                answer[1] = visited[x];
-            }
-        }
-        return answer;
-    }
+
 }
