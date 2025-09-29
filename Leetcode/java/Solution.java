9d9958d1eb9b009da1e7db99373418c7b78adf1b
dh1010a <dh1010a@naver.com>
2025-09-29 23:10:47 +0900

    add: leet746

--- Leetcode/java/Solution.java
+++ Leetcode/java/Solution.java
@@ -1,51 +1,14 @@
 import java.util.*;
 
 class Solution {
-	public int trap(int[] height) {
-		Deque<Node> dq = new ArrayDeque<>();
-		int[] trap = height.clone();
-		int answer = 0;
+	public int minCostClimbingStairs(int[] cost) {
+		int[] dp = new int[cost.length];
 
-		Node now = new Node(0, 0);
-
-		for (int i = 0; i < height.length; i++) {
-			int h = height[i];
-
-			while (!dq.isEmpty() && h >= dq.peekLast().h) {
-				now = dq.removeLast();
-			}
-
-			Node left;
-
-			if (dq.isEmpty()) {
-				left = now;
-				for (int j = left.idx + 1; j < i; j++) {
-					trap[j] = left.h;
-				}
-			} else {
-				left = dq.peekLast();
-				for (int j = left.idx + 1; j < i; j++) {
-					trap[j] = h;
-				}
-
-			}
-
-			dq.add(new Node(height[i], i));
+		dp[0] = cost[0];
+		dp[1] = cost[1];
+		for (int i = 2; i < cost.length; i++) {
+			dp[i] = Math.min(dp[i], dp[i - 1]) + cost[i];
 		}
-
-		for (int i = 0; i < height.length; i++) {
-			answer += trap[i] - height[i];
-		}
-		return answer;
-	}
-
-	class Node {
-		int h;
-		int idx;
-
-		public Node(int h, int idx) {
-			this.h = h;
-			this.idx = idx;
-		}
+		return dp[cost.length - 1];
 	}
 }
