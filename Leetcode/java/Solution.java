1255a5adb66e43e072bf6486b5149fd1bf83a7b9
dh1010a <dh1010a@naver.com>
2025-10-13 23:50:27 +0900

    add: pgs11421

--- Leetcode/java/Solution.java
+++ Leetcode/java/Solution.java
@@ -1,14 +1,58 @@
-import java.util.*;
+import java.util.Arrays;
 
 class Solution {
-	public int minCostClimbingStairs(int[] cost) {
-		int[] dp = new int[cost.length];
 
-		dp[0] = cost[0];
-		dp[1] = cost[1];
-		for (int i = 2; i < cost.length; i++) {
-			dp[i] = Math.min(dp[i], dp[i - 1]) + cost[i];
+	static int INF = 30001;
+
+	public int solution(int alp, int cop, int[][] problems) {
+		int maxAlp = 0;
+		int maxCop = 0;
+
+		for (int[] prb : problems) {
+			maxAlp = Math.max(maxAlp, prb[0]);
+			maxCop = Math.max(maxCop, prb[1]);
 		}
-		return dp[cost.length - 1];
+
+		if (alp >= maxAlp && cop >= maxCop) {
+			return 0;
+		}
+
+		maxAlp = Math.max(maxAlp, alp);
+		maxCop = Math.max(maxCop, cop);
+
+
+		int[][] dp = new int[maxAlp + 1][maxCop + 1];
+
+		for (int[] x : dp) {
+			Arrays.fill(x, INF);
+		}
+
+		dp[alp][cop] = 0;
+
+		for (int i = alp; i <= maxAlp; i++) {
+			for (int j = cop; j <= maxCop; j++) {
+				int now = dp[i][j];
+
+				if (i + 1 <= maxAlp) {
+					dp[i + 1][j] = Math.min(dp[i + 1][j], now + 1);
+				}
+
+				if (j + 1 <= maxCop) {
+					dp[i][j + 1] = Math.min(dp[i][j + 1], now + 1);
+				}
+
+				for (int[] prb : problems) {
+					if (prb[0] <= i && prb[1] <= j) {
+						int na = Math.min(maxAlp, i + prb[2]);
+						int nc = Math.min(maxCop, j + prb[3]);
+
+						dp[na][nc] = Math.min(dp[na][nc], now + prb[4]);
+					}
+				}
+
+			}
+		}
+
+		return dp[maxAlp][maxCop];
 	}
 }
